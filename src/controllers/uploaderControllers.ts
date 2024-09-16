27c2de59d279bcba6a03b048e2ef9838a3dd505a
BentlyM <metayerbently@gmail.com>
2024-09-15 21:46:05 -0400

    added register and login pages

--- src/controllers/uploaderControllers.ts
+++ src/controllers/uploaderControllers.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
+// home
 export const GET_home  = (req: Request, res: Response) => {
     res.render('home'); 
+}
+
+
+// register
+export const GET_register = (req: Request, res: Response) => {
+    const error = undefined;
+    res.render('register', {error});
+}
+
+
+
+// login
+export const GET_login = (req: Request, res: Response) => {
+    const error = undefined;
+    res.render('login', {error});
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,22 @@
 import express from 'express';
-import { GET_home } from '../controllers/uploaderControllers';
+import { GET_home, GET_login, GET_register } from '../controllers/uploaderControllers';
+import passport from 'passport';
+import '../strategies/local-strategy'
 
 
 const router = express.Router();
 
+//home
 router.get('/', GET_home);
+
+//login
+router.get('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}) , GET_login);
+
+
+//register
+router.get('/register', GET_register);
 
 export default router;
