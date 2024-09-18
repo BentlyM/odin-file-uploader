e607d43d0e987083718bd109ba80486af4ac6856
BentlyM <metayerbently@gmail.com>
2024-09-18 10:25:13 -0400

    added more specificatins on file config

--- src/controllers/uploaderControllers.ts
+++ src/controllers/uploaderControllers.ts
@@ -1,5 +1,6 @@
-import {Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { supabase } from '../supabase';
+import { v4 as uuidv4 } from 'uuid';
 
 // home
 export const GET_home = (req: Request, res: Response) => {
@@ -16,21 +17,32 @@ export const GET_home = (req: Request, res: Response) => {
 };
 
 export const POST_home = async (req: Request, res: Response) => {
-  const file = req.file;
+  try {
+    const file = req.file;
 
-  if (!file) {
-    return res.status(400).json({ msg: "Please upload files" });
+    if (!file) {
+      return res.status(400).json({ msg: 'Please upload files' });
+    }
+
+    const { originalname, buffer } = file;
+    const filename = `${uuidv4()}_${originalname}`;
+
+    const { data, error } = await supabase.storage
+      .from('documentation')
+      .upload(filename, buffer);
+
+    const fileUrl = `${process.env.SUPABASE_PROJECT_URL}/storage/v1/object/public/documentation/${filename}`;
+
+    if (error) {
+      return res
+        .status(500)
+        .json({ msg: 'Upload failed', error: error.message });
+    }
+
+    return res
+      .status(200)
+      .json({ msg: 'File uploaded successfully', uploaded_file: { data, fileUrl } });
+  } catch (err: any) {
+    return res.status(500).json({ err: err.message });
   }
-
-  const { originalname, buffer } = file;
-
-  const { data, error } = await supabase.storage
-    .from('documentation')
-    .upload(originalname, buffer);
-
-  if (error) {
-    return res.status(500).json({ msg: "Upload failed", error: error.message });
-  }
-
-  return res.status(200).json({ msg: "File uploaded successfully", data });
 };
