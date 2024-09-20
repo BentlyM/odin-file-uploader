20dd9333ddda335735a7c55cffab0ce10142d8dd
BentlyM <metayerbently@gmail.com>
2024-09-20 17:45:26 -0400

    added error handling

--- src/controllers/pagesControllers.ts
+++ src/controllers/pagesControllers.ts
@@ -3,27 +3,35 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// home
+// Home
 export const GET_home = async (req: Request, res: Response) => {
-  const currentUser = req.user as {id : number};
-    if (!currentUser) return res.redirect('/login');
+  const currentUser = req.user as { id: number }; // Ensure the user type is correctly defined
 
+  if (!currentUser) {
+    return res.redirect('/login');
+  }
+
+  try {
     const uploadedFiles = await prisma.file.findMany({
       where: {
         userId: currentUser.id,
         folderId: null,
-      }
+      },
     });
 
     const folders = await prisma.folder.findMany({
       where: {
         userId: currentUser.id,
-      }
-    })
+      },
+    });
 
     res.render('home', {
       user: req.user,
       uploadedFiles,
-      folders: folders
+      folders,
     });
-  };
+  } catch (error) {
+    console.error('Error fetching data for home:', error);
+    res.status(500).render('error', { message: 'Internal Server Error' });
+  }
+};
